e909f55188ffa4bdf4565159263715bae59d3a5d
morganweiss33 <47132851+morganweiss33@users.noreply.github.com>
2020-04-21 13:29:52 -0400

    Update and rename plain-oldJS.js to thingworx.js

---
+++ thingworx.js
@@ -0,0 +1,42 @@
+
+var dataParams = {
+	proxyScheme: undefined /* STRING */,
+	headers: undefined /* JSON */,
+	ignoreSSLErrors: true /* BOOLEAN */,
+	useNTLM: undefined /* BOOLEAN */,
+	workstation: undefined /* STRING */,
+	useProxy: undefined /* BOOLEAN */,
+	withCookies: undefined /* BOOLEAN */,
+	proxyHost: undefined /* STRING */,
+	url: "https://www.morgankweiss.com/img/bookData.json" /* STRING */,
+	timeout: undefined /* NUMBER */,
+	proxyPort: undefined /* INTEGER */,
+	password: undefined /* STRING */,
+	domain: undefined /* STRING */,
+	username: undefined /* STRING */
+};
+
+// result: JSON
+var data = Resources["ContentLoaderFunctions"].GetJSON(dataParams);
+
+
+
+
+var params = {
+    infoTableName : "InfoTable",
+    dataShapeName : "book-data"
+};
+
+// CreateInfoTableFromDataShape(infoTableName:STRING("InfoTable"), dataShapeName:STRING):INFOTABLE(book-data)
+var InfoTable = Resources["InfoTableFunctions"].CreateInfoTableFromDataShape(params);
+    //console.log(i);
+
+//for each(var books in myInfoTable.rows)
+
+	for (var i=0; i < data.array.length;i++) {    
+        //console.log("test");
+    InfoTable.AddRow({isbn:data.array[i].isbn, book_title:data.array[i].book_title, author:data.array[i].author});
+	}
+
+
+var result = InfoTable;
